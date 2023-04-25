a54b011048525d678e3b5b8c351024641cb7bcaf
Frieder Singer <friedersinger@users.noreply.github.com>
2023-04-25 10:22:09 +0200

    rest script

--- script.js
+++ script.js
@@ -21,6 +21,14 @@ let blocks = [
   },
 ];
 
+// let isSoundOn = true;
+
+// let soundButton = document.getElementById("sound-button");
+
+// soundButton.addEventListener("click", function () {
+//   isSoundOn = !isSoundOn;
+// });
+
 let counter = 0;
 let allPokemon = [];
 let AUDIO_PIKA = new Audio("audio/pokemon.mp3");
@@ -100,4 +108,97 @@ function renderPokemon(name, typeOne, typeTwo, img, id) {
   // Get the DOM element for the current Pokemon and add the class for its primary type.
   let pokemon = document.getElementById(name);
   pokemon.classList.add(typeOne);
+}
+
+function typeIsVoid(typeTwo, id) {
+  // Check if the second type is an empty string.
+  if (typeTwo == "") {
+    // If the second type is empty, get the DOM element with the corresponding ID and add the 'd-none' class to hide it.
+    document.getElementById(id).classList.add("d-none");
+  }
+}
+
+// This is a function called pokedexHTML that takes in various data about a Pokemon as arguments and returns an HTML string.
+function pokedexHTML(name, typeOne, typeTwo, img, id) {
+  // Return an HTML string that includes the name, types, ID, and image of the current Pokemon object.
+  return /*html*/ `
+          <div id="${name}" class="pokemon" onclick="showStats(${id})">
+              <div class="flexing">
+                  <h2>${name}</h2>
+                  <div>
+                      <h4 class="${typeOne}">${typeOne}</h4>
+                      <h4 id="${id}" class="${typeOne}">${typeTwo}</h4>
+                  </div>
+              </div>
+              <div class="imageContainer">
+                  <h4><b>#${id}</b></h4>
+                  <img class="pokemonImage" src="${img}">
+              </div>
+          </div>
+      `;
+}
+
+function loadNext() {
+  if (counter < 3) {
+    counter++;
+    loadPokemon();
+  } else if (counter == 3) {
+    counter++;
+    hideButton();
+    loadPokemon();
+  }
+}
+
+function hideButton() {
+  document.getElementById("hide").classList.add("d-none");
+}
+
+// This function is called when the user inputs a search query.
+function input() {
+  // Get the search query from the input element and convert it to uppercase for case-insensitive matching.
+  let search = document.getElementById("search").value;
+  search = search.toUpperCase();
+  // Clear the previous search results from the Pokedex element.
+  document.getElementById("pokedex").innerHTML = "";
+  // Loop through all the Pokemon in the allPokemon array.
+  for (let i = 0; i < allPokemon.length; i++) {
+    // Get the current Pokemon.
+    const pokemon = allPokemon[i];
+    // Check if the Pokemon's name matches the search query (case-insensitive).
+    if (pokemon["name"].toUpperCase().includes(search)) {
+      // If there is a match, display the Pokemon's info using the pokeInfo() function.
+      pokeInfo(pokemon);
+    }
+  }
+}
+
+// This function filters the displayed Pokemon by their type
+function selectedType(type) {
+  // Clear the existing Pokemon list to prepare for filtering
+  document.getElementById("pokedex").innerHTML = "";
+
+  // Loop through all Pokemon in the "allPokemon" array
+  for (let i = 0; i < allPokemon.length; i++) {
+    let pokemon = allPokemon[i];
+
+    // If the Pokemon's primary type matches the selected type, display it
+    if (pokemon["types"]["0"]["type"]["name"].includes(type)) {
+      pokeInfo(pokemon);
+
+      // If the Pokemon has a secondary type and it matches the selected type, display it
+    } else if (
+      pokemon["types"].length > 1 &&
+      pokemon["types"]["1"]["type"]["name"].includes(type)
+    ) {
+      pokeInfo(pokemon);
+
+      // If the selected type is "all", display all Pokemon
+    } else if (type == "all") {
+      pokeInfo(pokemon);
+    }
+  }
+}
+
+function errorFunction() {
+  console.log("Fehler aufgetreten");
 }
