bccfac303ae5c9c7474438c8c8269e731aa83183
Frieder Singer <friedersinger@users.noreply.github.com>
2023-07-03 22:39:27 +0200

    JSDoc / cleaner code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+/**
+ * An array of objects representing blocks of Pokemon.
+ * Each block has a start and end index.
+ * @type {Array<Object>}
+ */
 let blocks = [
   {
     start: 1,
@@ -21,18 +26,27 @@ let blocks = [
   },
 ];
 
-// let isSoundOn = true;
-
-// let soundButton = document.getElementById("sound-button");
-
-// soundButton.addEventListener("click", function () {
-//   isSoundOn = !isSoundOn;
-// });
-
+/**
+ * A counter to keep track of the current block being loaded.
+ * @type {number}
+ */
 let counter = 0;
+
+/**
+ * An array to store all the loaded Pokemon data.
+ * @type {Array<Object>}
+ */
 let allPokemon = [];
+
+/**
+ * The audio object for playing the Pikachu sound.
+ * @type {Audio}
+ */
 let AUDIO_PIKA = new Audio("audio/pokemon.mp3");
 
+/**
+ * Plays the Pikachu sound and hides the start button.
+ */
 function join() {
   AUDIO_PIKA.volume = 0.08;
   AUDIO_PIKA.play();
@@ -42,6 +56,11 @@ function join() {
   document.getElementById("body").style.overflow = "";
 }
 
+/**
+ * Loads the Pokemon data for the current block.
+ * Fetches data for each Pokemon in the block sequentially.
+ * @returns {Promise<void>}
+ */
 async function loadPokemon() {
   let blockStart = blocks[counter]["start"];
   let blockEnd = blocks[counter]["end"];
@@ -50,77 +69,83 @@ async function loadPokemon() {
   }
 }
 
-// This is an asynchronous function that loads data about a Pokemon from the PokeAPI based on its ID.
+/**
+ * Loads Pokemon data from the PokeAPI for the given index.
+ * @param {number} i - The index of the Pokemon to load.
+ * @returns {Promise<void>}
+ */
 async function loadFromNet(i) {
-  // Construct a URL string to access the PokeAPI for the given Pokemon ID.
   let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-  // Make a request to the PokeAPI using the URL and await the response.
   let response = await fetch(url).catch(errorFunction);
-  // Extract the JSON data from the response and await its parsing.
   let currentPokemon = await response.json().catch(errorFunction);
-  // Add the current Pokemon object to an array of all Pokemon.
   allPokemon.push(currentPokemon);
-  // Call the pokeInfo function with the current Pokemon object and ID.
   pokeInfo(currentPokemon, i);
-  // Call the input function to continue the program.
   input();
 }
 
-// This is a function called pokeInfo that takes in a Pokemon object as its argument.
+/**
+ * Processes the Pokemon data and renders it on the page.
+ * @param {Object} currentPokemon - The data of the current Pokemon.
+ */
 function pokeInfo(currentPokemon) {
-  // Extract the ID of the current Pokemon object and store it in a variable.
   let id = currentPokemon["id"];
-  // Extract the name of the current Pokemon object, capitalize the first letter, and store it in a variable.
   let name =
     currentPokemon["name"].charAt(0).toUpperCase() +
     currentPokemon["name"].slice(1);
-  // Extract the first type of the current Pokemon object, capitalize the first letter, and store it in a variable.
   let typeOne =
     currentPokemon["types"]["0"]["type"]["name"].charAt(0).toUpperCase() +
     currentPokemon["types"]["0"]["type"]["name"].slice(1);
-  // Declare a variable to hold the second type of the current Pokemon object.
   let typeTwo;
-  // Check if the current Pokemon has a second type.
   let typesLength = currentPokemon["types"].length;
   if (typesLength > 1) {
-    // If the Pokemon has a second type, extract it, capitalize the first letter, and store it in the typeTwo variable.
     typeTwo =
       currentPokemon["types"]["1"]["type"]["name"].charAt(0).toUpperCase() +
       currentPokemon["types"]["1"]["type"]["name"].slice(1);
   } else {
-    // If the Pokemon does not have a second type, set typeTwo to an empty string.
     typeTwo = "";
   }
-  // Extract the URL for the official artwork of the current Pokemon object and store it in a variable called img.
   let img =
     currentPokemon["sprites"]["other"]["official-artwork"]["front_default"];
-  // Call the renderPokemon function with the name, types, image, and ID of the current Pokemon object.
   renderPokemon(name, typeOne, typeTwo, img, id);
 }
 
-// This is a function called renderPokemon that takes in various data about a Pokemon as arguments.
+/**
+ * Renders the Pokemon information on the page.
+ * @param {string} name - The name of the Pokemon.
+ * @param {string} typeOne - The primary type of the Pokemon.
+ * @param {string} typeTwo - The secondary type of the Pokemon.
+ * @param {string} img - The URL of the Pokemon's image.
+ * @param {number} id - The ID of the Pokemon.
+ */
 function renderPokemon(name, typeOne, typeTwo, img, id) {
-  // Get the DOM element for the Pokedex container and append the HTML for the current Pokemon to it.
   let pokedex = document.getElementById("pokedex");
   pokedex.innerHTML += pokedexHTML(name, typeOne, typeTwo, img, id);
-  // Check if the second type of the current Pokemon is null or undefined and add the 'void' class to the corresponding element in the HTML.
   typeIsVoid(typeTwo, id);
-  // Get the DOM element for the current Pokemon and add the class for its primary type.
   let pokemon = document.getElementById(name);
   pokemon.classList.add(typeOne);
 }
 
+/**
+ * Checks if the secondary type is void and hides the corresponding element.
+ * @param {string} typeTwo - The secondary type of the Pokemon.
+ * @param {number} id - The ID of the Pokemon.
+ */
 function typeIsVoid(typeTwo, id) {
-  // Check if the second type is an empty string.
   if (typeTwo == "") {
-    // If the second type is empty, get the DOM element with the corresponding ID and add the 'd-none' class to hide it.
     document.getElementById(id).classList.add("d-none");
   }
 }
 
-// This is a function called pokedexHTML that takes in various data about a Pokemon as arguments and returns an HTML string.
+/**
+ * Generates the HTML markup for displaying a Pokemon in the Pokedex.
+ * @param {string} name - The name of the Pokemon.
+ * @param {string} typeOne - The primary type of the Pokemon.
+ * @param {string} typeTwo - The secondary type of the Pokemon.
+ * @param {string} img - The URL of the Pokemon's image.
+ * @param {number} id - The ID of the Pokemon.
+ * @returns {string} The HTML markup for displaying the Pokemon.
+ */
 function pokedexHTML(name, typeOne, typeTwo, img, id) {
-  // Return an HTML string that includes the name, types, ID, and image of the current Pokemon object.
   return /*html*/ `
           <div id="${name}" class="pokemon" onclick="showStats(${id})">
               <div class="flexing">
@@ -138,8 +163,16 @@ function pokedexHTML(name, typeOne, typeTwo, img, id) {
       `;
 }
 
+/**
+ * The audio object for playing the next sound.
+ * @type {Audio}
+ */
 let AUDIO_NEXT = new Audio("audio/next.mp3");
 
+/**
+ * Loads the next block of Pokemon data.
+ * Plays the next sound effect and updates the counter.
+ */
 function loadNext() {
   AUDIO_NEXT.volume = 0.08;
   AUDIO_NEXT.play();
@@ -153,56 +186,52 @@ function loadNext() {
   }
 }
 
+/**
+ * Hides the "Load More" button.
+ */
 function hideButton() {
   document.getElementById("hide").classList.add("d-none");
 }
 
-// This function is called when the user inputs a search query.
+/**
+ * Handles the search input and filters the displayed Pokemon accordingly.
+ */
 function input() {
-  // Get the search query from the input element and convert it to uppercase for case-insensitive matching.
   let search = document.getElementById("search").value;
   search = search.toUpperCase();
-  // Clear the previous search results from the Pokedex element.
   document.getElementById("pokedex").innerHTML = "";
-  // Loop through all the Pokemon in the allPokemon array.
   for (let i = 0; i < allPokemon.length; i++) {
-    // Get the current Pokemon.
     const pokemon = allPokemon[i];
-    // Check if the Pokemon's name matches the search query (case-insensitive).
     if (pokemon["name"].toUpperCase().includes(search)) {
-      // If there is a match, display the Pokemon's info using the pokeInfo() function.
       pokeInfo(pokemon);
     }
   }
 }
 
-// This function filters the displayed Pokemon by their type
+/**
+ * Filters the displayed Pokemon based on the selected type.
+ * @param {string} type - The selected type.
+ */
 function selectedType(type) {
-  // Clear the existing Pokemon list to prepare for filtering
   document.getElementById("pokedex").innerHTML = "";
-
-  // Loop through all Pokemon in the "allPokemon" array
   for (let i = 0; i < allPokemon.length; i++) {
     let pokemon = allPokemon[i];
-
-    // If the Pokemon's primary type matches the selected type, display it
     if (pokemon["types"]["0"]["type"]["name"].includes(type)) {
       pokeInfo(pokemon);
-
-      // If the Pokemon has a secondary type and it matches the selected type, display it
     } else if (
       pokemon["types"].length > 1 &&
       pokemon["types"]["1"]["type"]["name"].includes(type)
     ) {
       pokeInfo(pokemon);
-
-      // If the selected type is "all", display all Pokemon
     } else if (type == "all") {
       pokeInfo(pokemon);
     }
   }
 }
 
+/**
+ * An error handling function.
+ */
 function errorFunction() {
-  console.log("Fehler aufgetreten");
+  console.log("An error occurred");
 }
--- js/stats.js
+++ js/stats.js
@@ -1,3 +1,7 @@
+/**
+ * An array of background color configurations.
+ * @type {string[]}
+ */
 const CONFIG_BG_COLOR = [
   "rgba(255, 99, 132, 0.2)",
   "rgba(255, 0, 0, 0.2)",
@@ -7,6 +11,10 @@ const CONFIG_BG_COLOR = [
   "rgba(255, 255, 0, 0.2)",
 ];
 
+/**
+ * An array of border color configurations.
+ * @type {string[]}
+ */
 const CONFIG_BORDER_COLOR = [
   "rgba(255, 99, 132, 1)",
   "rgba(255, 0, 0, 1)",
@@ -16,86 +24,114 @@ const CONFIG_BORDER_COLOR = [
   "rgba(255, 255, 0, 1)",
 ];
 
-let AUDIO_CLICK = new Audio("audio/pokemon-found.mp3");
+/**
+ * Audio element for click sound.
+ * @type {HTMLAudioElement}
+ */
+const AUDIO_CLICK = new Audio("audio/pokemon-found.mp3");
 
+/**
+ * Shows the stats for a Pokemon based on its ID.
+ * @param {number} id - The ID of the Pokemon.
+ */
 function showStats(id) {
   AUDIO_CLICK.volume = 0.08;
   AUDIO_CLICK.play();
-  let currentPokemon = allPokemon[id - 1];
-  let previousPokemon = allPokemon[id - 2];
-  let nextPokemon = allPokemon[id];
-  bluryBackground(); // Show the blurry background
-  renderStatsContainer(id); // Render the stats container
-  currentStats(currentPokemon); // Display the stats for the current Pokemon
-  setChangeImages(previousPokemon, nextPokemon); // Set the previous and next Pokemon images
-  toggleActiveTab("aboutTab"); // Toggle the "about" tab as active
+  const currentPokemon = allPokemon[id - 1];
+  const previousPokemon = allPokemon[id - 2];
+  const nextPokemon = allPokemon[id];
+  bluryBackground();
+  renderStatsContainer(id);
+  currentStats(currentPokemon);
+  setChangeImages(previousPokemon, nextPokemon);
+  toggleActiveTab("aboutTab");
 }
 
-// Shows the blurry background
+/**
+ * Blurs the background and prevents scrolling.
+ */
 function bluryBackground() {
-  let statsBackground = document.getElementById("bluryBackground");
+  const statsBackground = document.getElementById("bluryBackground");
   statsBackground.innerHTML = bluryBackgroundHTML();
   document.getElementById("body").style.overflow = "hidden";
 }
 
-// Returns the HTML for the blurry background
+/**
+ * Generates the HTML for the blury background.
+ * @returns {string} The HTML string for the blury background.
+ */
 function bluryBackgroundHTML() {
   return /*html*/ `
-          <div id="statsBackground" class="statsBackground" onclick="showAll()">
-  
-          </div>
-      `;
+    <div id="statsBackground" class="statsBackground" onclick="showAll()"></div>
+  `;
 }
 
-// Renders the stats container for the current Pokemon
+/**
+ * Renders the stats container for the Pokemon.
+ * @param {number} id - The ID of the Pokemon.
+ */
 function renderStatsContainer(id) {
-  let colorClass =
+  const colorClass =
     allPokemon[id - 1]["types"]["0"]["type"]["name"].charAt(0).toUpperCase() +
     allPokemon[id - 1]["types"]["0"]["type"]["name"].slice(1);
-  let statsContainer = document.getElementById("statsContainer");
+  const statsContainer = document.getElementById("statsContainer");
   statsContainer.innerHTML = statsContainerHTML(id);
   document.getElementById("statsContainer").classList.remove("statsHidden");
   document.getElementById("statsTop").classList.add(colorClass + "Clicked");
 }
 
-// Displays the stats for the current Pokemon
+/**
+ * Shows the current stats for the Pokemon.
+ * @param {Object} currentPokemon - The current Pokemon object.
+ */
 function currentStats(currentPokemon) {
-  let id = currentPokemon["id"];
-  let name =
+  const id = currentPokemon["id"];
+  const name =
     currentPokemon["name"].charAt(0).toUpperCase() +
     currentPokemon["name"].slice(1);
-  let image =
+  const image =
     currentPokemon["sprites"]["other"]["dream_world"]["front_default"];
-  let height = (currentPokemon["height"] / 10).toFixed(2);
-  let weight = (currentPokemon["weight"] / 10).toFixed(1);
-  let heldItems = currentPokemon["held_items"];
-  let abilities = currentPokemon["abilities"];
+  const height = (currentPokemon["height"] / 10).toFixed(2);
+  const weight = (currentPokemon["weight"] / 10).toFixed(1);
+  const heldItems = currentPokemon["held_items"];
+  const abilities = currentPokemon["abilities"];
   currentTypes(currentPokemon, name, id);
   controlItems(height, weight, heldItems, abilities);
   setBottomSection(image);
 }
 
-// Controls the display of the held items and abilities for the current Pokemon
+/**
+ * Controls the display of held items and abilities.
+ * @param {string} height - The height of the Pokemon.
+ * @param {string} weight - The weight of the Pokemon.
+ * @param {Object[]} heldItems - An array of held items.
+ * @param {Object[]} abilities - An array of abilities.
+ */
 function controlItems(height, weight, heldItems, abilities) {
+  const heldItemsElement = document.getElementById("heldItems");
   if (heldItems.length > 0) {
     for (let i = 0; i < heldItems.length; i++) {
       const heldItem = heldItems[i]["item"]["name"];
       if (i > 0) {
-        document.getElementById("heldItems").innerHTML += ",<br>";
+        heldItemsElement.innerHTML += ",<br>";
       }
-      document.getElementById("heldItems").innerHTML += heldItem;
+      heldItemsElement.innerHTML += heldItem;
     }
   } else {
-    document.getElementById("heldItems").innerHTML = "-/-";
+    heldItemsElement.innerHTML = "-/-";
   }
   controlAbilities(height, weight, abilities);
 }
 
-// This function takes in the height, weight, and abilities of a Pokemon and displays them on the page
+/**
+ * Controls the display of abilities.
+ * @param {string} height - The height of the Pokemon.
+ * @param {string} weight - The weight of the Pokemon.
+ * @param {Object[]} abilities - An array of abilities.
+ */
 function controlAbilities(height, weight, abilities) {
   for (let i = 0; i < abilities.length; i++) {
     const ability = abilities[i]["ability"]["name"];
-    // If this is not the first ability, add a comma and line break
     if (i > 0) {
       document.getElementById("abilities").innerHTML += ",<br>";
     }
@@ -104,40 +140,47 @@ function controlAbilities(height, weight, abilities) {
   setOthers(height, weight);
 }
 
-// This function displays the height and weight of a Pokemon on the page
+/**
+ * Sets the height and weight of the Pokemon.
+ * @param {string} height - The height of the Pokemon.
+ * @param {string} weight - The weight of the Pokemon.
+ */
 function setOthers(height, weight) {
   document.getElementById("height").innerHTML = `${height} m`;
   document.getElementById("weight").innerHTML = `${weight} kg`;
 }
 
 /**
- * Sets the current Pokemon's types and calls setTopSection and clickedTypeIsVoid functions.
+ * Shows the current types for the Pokemon.
  * @param {Object} currentPokemon - The current Pokemon object.
- * @param {string} name - The name of the current Pokemon.
- * @param {number} id - The ID of the current Pokemon.
+ * @param {string} name - The name of the Pokemon.
+ * @param {number} id - The ID of the Pokemon.
  */
 function currentTypes(currentPokemon, name, id) {
-  // Get the first type of the current Pokemon
   let typeOne =
     currentPokemon["types"]["0"]["type"]["name"].charAt(0).toUpperCase() +
     currentPokemon["types"]["0"]["type"]["name"].slice(1);
   let typeTwo;
   let types = currentPokemon["types"];
   if (types.length > 1) {
-    // If the current Pokemon has two types
-    // Get the second type of the current Pokemon
     typeTwo =
       currentPokemon["types"]["1"]["type"]["name"].charAt(0).toUpperCase() +
       currentPokemon["types"]["1"]["type"]["name"].slice(1);
-    setTopSection(name, id, typeOne, typeTwo); // Set the top section with both types
+    setTopSection(name, id, typeOne, typeTwo);
   } else {
     typeTwo = "";
-    setTopSection(name, id, typeOne, typeTwo); // Set the top section with the first type only
+    setTopSection(name, id, typeOne, typeTwo);
   }
-  clickedTypeIsVoid(typeTwo); // Check if the second type is empty and hide the element if so
+  clickedTypeIsVoid(typeTwo);
 }
 
-// This function sets the top section of the stats container, including the name, ID, and types of the clicked Pokemon
+/**
+ * Sets the top section of the stats container.
+ * @param {string} name - The name of the Pokemon.
+ * @param {number} id - The ID of the Pokemon.
+ * @param {string} typeOne - The primary type of the Pokemon.
+ * @param {string} typeTwo - The secondary type of the Pokemon.
+ */
 function setTopSection(name, id, typeOne, typeTwo) {
   document.getElementById("clickedName").innerHTML = name;
   document.getElementById("clickedId").innerHTML += id;
@@ -145,80 +188,100 @@ function setTopSection(name, id, typeOne, typeTwo) {
   document.getElementById("clickedTypeTwo").innerHTML = typeTwo;
 }
 
+/**
+ * Sets the bottom section of the stats container.
+ * @param {string} image - The URL of the Pokemon's image.
+ */
 function setBottomSection(image) {
   document.getElementById("clickedImage").src = image;
 }
 
+/**
+ * Checks if the secondary type is void and hides it if necessary.
+ * @param {string} typeTwo - The secondary type of the Pokemon.
+ */
 function clickedTypeIsVoid(typeTwo) {
   if (typeTwo == "") {
     document.getElementById("clickedTypeTwo").classList.add("d-none");
   }
 }
 
+/**
+ * Generates the HTML code for the stats container.
+ * @param {number} id - The ID of the Pokemon.
+ * @returns {string} The HTML code for the stats container.
+ */
 function statsContainerHTML(id) {
   return /*html*/ `
     <div>
-        <div id="statsTop" class="statsTop">
-            <div class="nameIdArea">
-                <h2 id="clickedName"></h2>
-                <h4 id="clickedId">#</h4>
-            </div>
-            <div class="typeArea">
-                <h4 id="clickedTypeOne"></h4>
-                <h4 id="clickedTypeTwo"></h4>
-            </div>
+      <div id="statsTop" class="statsTop">
+        <div class="nameIdArea">
+          <h2 id="clickedName"></h2>
+          <h4 id="clickedId">#</h4>
         </div>
-        <div id="statsBottom" class="statsBottom">
-            <div class="imageToCenter">
-                <img class=clickedImage id="clickedImage">
-            </div>
-            <div class="changeStatsArea">
-                <img id="previousImage" class="smallPNG left" onclick="showStats(${
-                  id - 1
-                })">
-                <div class="navArea">
-                    <p onclick="showStats(${id})" id="aboutTab">ABOUT</p>
-                    <p onclick="statsSection(${id})" id="statsTab">BASE STATS</p>
-                    <p onclick="movesSection(${id})" id="movesTab">MOVES</p>
-                </div>
-                <img id="nextImage" class="smallPNG right" onclick="showStats(${
-                  id + 1
-                })">
-            </div>
-            <div id="section">
-                <table>
-                    <tr>
-                        <th>Height:</th>
-                        <td id="height"></td>
-                    </tr>
-                    <tr>
-                        <th>Weight:</th>
-                        <td id="weight"></td>
-                    </tr>
-                    <tr>
-                        <th>Held Items:</th>
-                        <td id="heldItems"></td>
-                    </tr>
-                    <tr>
-                        <th>Abilities:</th>
-                        <td id="abilities"></td>
-                    </tr>
-                </table>
-            </div>
-            <div class="closeButton">
-                <button class="btn btn-secondary" type="submit" onclick="showAll()">Close</button>
-            </div>
+        <div class="typeArea">
+          <h4 id="clickedTypeOne"></h4>
+          <h4 id="clickedTypeTwo"></h4>
         </div>
+      </div>
+      <div id="statsBottom" class="statsBottom">
+        <div class="imageToCenter">
+          <img class="clickedImage" id="clickedImage">
+        </div>
+        <div class="changeStatsArea">
+          <img id="previousImage" class="smallPNG left" onclick="showStats(${
+            id - 1
+          })">
+          <div class="navArea">
+            <p onclick="showStats(${id})" id="aboutTab">ABOUT</p>
+            <p onclick="statsSection(${id})" id="statsTab">BASE STATS</p>
+            <p onclick="movesSection(${id})" id="movesTab">MOVES</p>
+          </div>
+          <img id="nextImage" class="smallPNG right" onclick="showStats(${
+            id + 1
+          })">
+        </div>
+        <div id="section">
+          <table>
+            <tr>
+              <th>Height:</th>
+              <td id="height"></td>
+            </tr>
+            <tr>
+              <th>Weight:</th>
+              <td id="weight"></td>
+            </tr>
+            <tr>
+              <th>Held Items:</th>
+              <td id="heldItems"></td>
+            </tr>
+            <tr>
+              <th>Abilities:</th>
+              <td id="abilities"></td>
+            </tr>
+          </table>
+        </div>
+        <div class="closeButton">
+          <button class="btn btn-secondary" type="submit" onclick="showAll()">Close</button>
+        </div>
+      </div>
     </div>
-    `;
+  `;
 }
-
+/**
+ * Hides the stats container and restores the page scrollability.
+ */
 function showAll() {
   document.getElementById("statsBackground").classList.add("d-none");
   document.getElementById("body").style.overflow = "";
   document.getElementById("statsContainer").classList.add("statsHidden");
 }
 
+/**
+ * Sets the previous and next Pokemon images in the stats container.
+ * @param {Object} previousPokemon - The previous Pokemon object.
+ * @param {Object} nextPokemon - The next Pokemon object.
+ */
 function setChangeImages(previousPokemon, nextPokemon) {
   if (previousPokemon == undefined) {
     document.getElementById("previousImage").classList.add("d-none");
@@ -235,6 +298,10 @@ function setChangeImages(previousPokemon, nextPokemon) {
   }
 }
 
+/**
+ * Displays the stats section for a given Pokemon ID.
+ * @param {number} id - The ID of the Pokemon.
+ */
 function statsSection(id) {
   let hpValue = allPokemon[id - 1]["stats"]["0"]["base_stat"];
   let hpName = allPokemon[id - 1]["stats"]["0"]["stat"]["name"];
@@ -265,6 +332,21 @@ function statsSection(id) {
   toggleActiveTab("statsTab");
 }
 
+/**
+ * Renders the HTML for the stats section.
+ * @param {number} hpValue - The HP stat value.
+ * @param {string} hpName - The HP stat name.
+ * @param {number} attackValue - The Attack stat value.
+ * @param {string} attackName - The Attack stat name.
+ * @param {number} defenseValue - The Defense stat value.
+ * @param {string} defenseName - The Defense stat name.
+ * @param {number} spAttackValue - The Special Attack stat value.
+ * @param {string} spAttackName - The Special Attack stat name.
+ * @param {number} spDefenseValue - The Special Defense stat value.
+ * @param {string} spDefenseName - The Special Defense stat name.
+ * @param {number} speedValue - The Speed stat value.
+ * @param {string} speedName - The Speed stat name.
+ */
 function statsHTML(
   hpValue,
   hpName,
@@ -322,6 +404,10 @@ function statsHTML(
   });
 }
 
+/**
+ * Displays the moves section for a given Pokemon ID.
+ * @param {number} id - The ID of the Pokemon.
+ */
 function movesSection(id) {
   let allMoves = allPokemon[id - 1]["moves"];
   document.getElementById("section").innerHTML = "";
@@ -336,6 +422,10 @@ function movesSection(id) {
   toggleActiveTab("movesTab");
 }
 
+/**
+ * Toggles the active tab.
+ * @param {string} tabId - The ID of the tab to activate.
+ */
 function toggleActiveTab(tabId) {
   const tabs = document.querySelectorAll(".navArea p");
   tabs.forEach((tab) => {
